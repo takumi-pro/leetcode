796c662c5e323c59174cc43920331503ef836b53
takumi-pro <takumidiary.0927@gmail.com>
2023-04-07 00:13:20 +0900

    two-sum

---
+++ 1.two-sum.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+  vector<int> twoSum(vector<int>& nums, int target) {
+    for (int i=0; i<nums.size(); i++) {
+      for (int j=i+1; j<nums.size(); j++) {
+        if (nums[i]+nums[j] == target) {
+          return {i, j};
+        }
+      }
+    }
+    return {};
+  }
+};
+// @lc code=end
+
