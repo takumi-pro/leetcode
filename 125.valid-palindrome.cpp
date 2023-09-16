3238cf5e8ef95c0866064466026fd4067c163856
takumi-pro <takumidiary.0927@gmail.com>
2023-09-16 14:26:51 +0900

    125.valid-palindrome

--- 125.valid-palindrome.cpp
+++ 125.valid-palindrome.cpp
@@ -8,7 +8,18 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        
+        auto it = remove_if(s.begin(), s.end(), [](char const &c) {
+            return !isalnum(c);
+        });
+        s.erase(it, s.end());
+
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+        int max_i = s.size() - 1;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] != s[max_i - i]) return false;
+        }
+        return true;
     }
 };
 // @lc code=end
